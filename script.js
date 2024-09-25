0dcb3416246feeed0623302f79f837dd68fc23f6
Ramos-Gomez-Jose-Antonio <jramosg2202@alumno.ipn.mx>
2024-09-24 18:49:21 -0600

    Creacion

---
+++ script.js
@@ -0,0 +1,28 @@
+//Clase 24/09/2024
+//Nombre: Ramos Gómez José Antonio
+
+//VARIABLES
+
+/*
+LAS VARIABLES GUARDAN INFORMACIÓN QUE PUEDES REUTILIZAR
+*/
+
+//Declaración de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor (bloques de código)
+const PI = 3.1416; // 
+var edad = 25; // Variable global para todo el código
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a- b; //3
+let multiplicacion = a * b; //10
+let division = a/b; //2,5
+
+//operadores de comparacion
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
