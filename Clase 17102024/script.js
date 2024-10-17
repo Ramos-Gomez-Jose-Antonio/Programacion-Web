564454186914d6caa1a909760c1110d72f9e12ee
Ramos-Gomez-Jose-Antonio <jramosg2202@alumno.ipn.mx>
2024-10-17 16:42:37 -0600

    Inicio de Sesion

---
+++ Clase 17102024/script.js
@@ -0,0 +1,15 @@
+document.title = 'Clase de JS';
+console.log(document.title);
+
+const h1 = document.createElement('h1');
+
+h1.textContent = "Mi Texto desde JS";
+document.body.appendChild(h1);           //para poder visualizarlo en el navegador IMPORTANTE: el body
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------/
+const parrafo = document.createElement('p');
+
+parrafo.textContent = "es confuso verdad? sin embargo skibidi mewing sigma esta mal, todo el globo de texto te lo hace saber, te notas chad con pensamientos de decadencia esa sensacion de compa soy un buen compa de que ya el prime no volvera a ser como antes, ya ya mijo ya, venga la alegria npc un video ma mi gente, insano heroico, bobicraft descuido, chamba goku god, ojolero wachi wachi wa, tito calderon, oh oh me esta comiendo buscar, ahora podria decirse, fuiste un insano sin saberlo"
+document.body.appendChild(parrafo);           //para poder visualizarlo en el navegador IMPORTANTE: el body
+
+//
